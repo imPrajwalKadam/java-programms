b77998ac1cf74778d5d964b2a430eb6690a60502
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-14 11:28:34 +0530

    Add files via upload

---
+++ program140.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class program140
+{
+    static int Addition(int x,int y)
+    {
+        return x+y;
+    }
+	public static void main(String arg[])
+	{
+		int no1,no2,ans;
+		Scanner sobj = new Scanner(System.in);
+
+		System.out.println("Enter first number");
+		no1 = sobj.nextInt();
+		
+		System.out.println("Enter second number");
+		no2 = sobj.nextInt();
+		
+		ans = Addition(no1,no2);
+		
+		System.out.println("Addition is : "+ans);
+	}
+}
