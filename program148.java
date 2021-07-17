b4c4331c2ee6e88f6d943df893a9bebe209c1100
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-17 22:35:24 +0530

    Add files via upload

---
+++ program148.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class StringX
+{
+	public void Display(String  str)
+	{
+		char arr[] = str.toCharArray();
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
+
+
+class program148
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter String");
+		String str = sobj.nextLine();
+		StringX obj  = new StringX();
+		obj.Display(str);
+	}
+}
