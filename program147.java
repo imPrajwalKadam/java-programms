2fe2c5d93c05676ddef933d023b0c9f575d6b7b4
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-07-17 22:34:56 +0530

    Add files via upload

---
+++ program147.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class program147
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter String");
+		String str = sobj.nextLine();
+		System.out.println("Entered string is:"+str);
+	}
+}
